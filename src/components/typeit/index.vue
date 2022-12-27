1a16f7c66df5a38f51a948ae3c6ed3201f2e366a
huangyul <1103221645@qq.com>
2022-12-27 16:02:57 +0800

    feat: 🎸 add element-plus

---
+++ src/components/typeit/index.vue
@@ -0,0 +1,4 @@
+<!-- 打字机组件 -->
+<script setup lang="ts"></script>
+
+<template></template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="login-box">login123</div>
+  <div class="login-box">login123
+    <el-button>1232</el-button>
+  </div>
 </template>
 
 
