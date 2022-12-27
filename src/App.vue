9d66fc79eb8973f0ef2cd769398235d7b71f613b
huangyul <1103221645@qq.com>
2022-12-27 14:55:39 +0800

    feat: 🎸 add vue-router and sass

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>vue admin</div>
+  <div class="title">vue admin</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.title {
+  color: red;
+}
+</style>
